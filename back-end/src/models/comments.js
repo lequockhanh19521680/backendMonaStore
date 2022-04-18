3b060071a13f73a377151ba406c577dede430865
khanhsd0901 <khanhsd0901@gmail.com>
2022-04-18 10:44:37 +0700

    thay doi model comment

--- back-end/src/models/comments.js
+++ back-end/src/models/comments.js
@@ -5,11 +5,13 @@ const userSchema = require('./user')
 const commentSchema = new mongoose.Schema({
     productId:{
         type: mongoose.Schema.Types.ObjectId,
-        ref:'Product'
+        ref:'Product',
+        required: true,
     },
     userId:{
         type: mongoose.Schema.Types.ObjectId,
-        ref:'User'
+        ref:'User',
+        required: true,
     },
     content:{
         type:String,
@@ -19,6 +21,10 @@ const commentSchema = new mongoose.Schema({
         min:1,
         max:5,
     },
+    createAt:{
+        type: Date,
+        default: Date.now
+    }
 })
 
 module.exports = mongoose.model('Comment',commentSchema);
