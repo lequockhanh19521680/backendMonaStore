7bd7b9a25118a5bbbf70ead0d8286816f34d7481
Duy Bao <duybao18112000@gmail.com>
2022-04-20 13:46:35 +0700

    add link to home

--- front-end/src/components/AdminHeader/AdminHeader.js
+++ front-end/src/components/AdminHeader/AdminHeader.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FiBell } from "react-icons/fi";
-
+import { Link } from 'react-router-dom'
 export default function AdminHeader() {
   return (
     <div className="w-full flex justify-between bg-dark-1 pl-5 pr-56 py-3 sticky top-0">
+       <Link to="/">
         <img src="/logo.png" width={150} alt="logo" />
+      </Link>
 
         <button className="text-green-1 font-bold text-2xl">
             <i className='bx bxs-bell'></i>
